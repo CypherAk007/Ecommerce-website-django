1818bbbdce67440ca095aceddb1edb2ce992d95a
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-03-16 17:56:12 +0530

    Basic Api routing without framework

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .products import products
 # Create your views here.
 
 def getRoutes(request):
@@ -15,4 +16,5 @@ def getRoutes(request):
     ]
     return JsonResponse(routes,safe=False)
 
-
+def getProducts(request):
+    return JsonResponse(products,safe=False)
