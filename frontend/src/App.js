afc638f490628a0020e048683dc04ba74c9aac9f
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-03-16 10:28:31 +0530

    Frontend: products and home page

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+// import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen';
+function App() {
+  return (
+    <Router>
+
+      <Header></Header>
+      <main className='py-3'>
+        <Container>
+          <Routes>
+              <Route path='/' element={<HomeScreen/>} exact/>     
+              <Route path='/product/:id' element={<ProductScreen/>} />     
+          </Routes>
+        </Container>
+      </main>
+      
+      <Footer></Footer>
+    </Router>
+  );
+}
+
+export default App;
