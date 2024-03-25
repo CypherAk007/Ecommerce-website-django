83c633037968c7fd16c0e7bcdaa9128ca5416b98
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-03-25 22:51:44 +0530

    Overriding users Model without Modifing the model using signals - pre_save signal

--- backend/base/signals.py
+++ backend/base/signals.py
@@ -6,6 +6,11 @@ from django.contrib.auth.models import User
 def updateUser(sender,instance,**kwargs):
     # sender - object that sends the signal 
     # instance - actual object 
+    # Here we are overriding user model 
+    # before saving username to db it saves username=email to db 
+    user = instance
+    if user.email!='':
+        user.username = user.email
     print('Signal Triggerd')
 
 pre_save.connect(updateUser,sender=User)
