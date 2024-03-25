cb339f0b87d67028d383d0e91cc7348f2af03d90
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-03-25 18:28:17 +0530

    typo fix

--- backend/base/views.py
+++ backend/base/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Product
-from .serailizers import ProductSerializer
+from .serializers import ProductSerializer
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -16,7 +16,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        
+
         data['username'] = self.user.username 
         data['email'] = self.user.email
 
@@ -39,12 +39,12 @@ def getRoutes(request):
 @api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
-    serilizer = ProductSerializer(products,many = True)
-    return Response(serilizer.data )
+    serializer = ProductSerializer(products,many = True)
+    return Response(serializer.data )
 
 
 @api_view(['GET'])
 def getProduct(request,pk):
     product = Product.objects.get(_id=pk)
-    serilizer = ProductSerializer(product,many=False)
-    return Response(serilizer.data)
+    serializer = ProductSerializer(product,many=False)
+    return Response(serializer.data)
