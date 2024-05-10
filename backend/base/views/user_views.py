cb608972f71f53482e60d25259b65d8dca173f7f
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-05-10 19:10:30 +0530

    Admin Update user and getUserById

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -86,6 +86,28 @@ def getUsers(request):
     serializer = UserSerializer(users,many = True)
     return Response(serializer.data )
 
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUserById(request,pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(user,many = False)
+    return Response(serializer.data )
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateUser(request,pk):
+    user = User.objects.get(id=pk)
+    
+    data  = request.data 
+    user.first_name = data['name']
+    user.username = data['email']
+    user.email = data['email']
+
+    user.save()
+
+    serializer = UserSerializer(user,many = False) #when we update the user we want to put new token in the local storage
+    return Response(serializer.data )
+
 
 @api_view(['DELETE'])
 @permission_classes([IsAdminUser])
